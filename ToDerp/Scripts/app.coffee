f49e8916ecda46592720d9d748fa7b257855c20a
swilliams <scott@krazyyak.com>
2013-01-22 21:42:47 -0700

    added collection methods

--- ToDerp/Scripts/app.coffee
+++ ToDerp/Scripts/app.coffee
@@ -6,6 +6,13 @@ class TodoItems extends Backbone.Collection
 	model: TodoItem
 	url: '/api/Todo'
 
+	inProgress: ->
+		@where Completed: false
+
+	completed: ->
+		@where Completed: true
+
+
 
 @derp = window.derp ? {}
 @derp.TodoItem = TodoItem
--- ToDerp/Scripts/app.js
+++ ToDerp/Scripts/app.js
@@ -1,4 +1,4 @@
-﻿(function() {
+(function() {
   var TodoItem, TodoItems, _ref,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
@@ -30,6 +30,18 @@
     TodoItems.prototype.model = TodoItem;
 
     TodoItems.prototype.url = '/api/Todo';
+
+    TodoItems.prototype.inProgress = function() {
+      return this.where({
+        Completed: false
+      });
+    };
+
+    TodoItems.prototype.completed = function() {
+      return this.where({
+        Completed: true
+      });
+    };
 
     return TodoItems;
 
